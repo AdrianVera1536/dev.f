8b638faaeb25deeddb3063158db00e1940e2cdd5
Adrian Vera <r05063958@gmail.com>
2025-03-02 22:48:45 -0600

    clase1

---
+++ index.js
@@ -0,0 +1,18 @@
+// Ejemplos de tipos de datos en JavaScript
+
+console.log(typeof 42); // number
+console.log(typeof 'Veinticinco'); // string
+console.log(typeof -666); // number
+console.log(typeof true); // boolean
+console.log(typeof 0); // number
+console.log(typeof ''); // string
+console.log(typeof null); // object (Esto es un comportamiento especial de JavaScript)
+console.log(typeof undefined); // undefined
+console.log(typeof false); // boolean
+
+// Ejemplos adicionales
+console.log(typeof 3.14); // number
+console.log(typeof { nombre: 'Juan' }); // object
+console.log(typeof [1, 2, 3]); // object (Arreglos son técnicamente objetos en JavaScript)
+console.log(typeof function() {}); // function
+console.log(typeof NaN); // number (NaN es un valor numérico especial)
